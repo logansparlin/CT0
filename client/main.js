77a839891d7f95895cbdceadcf0dbf8efbc7ec2e
logansparlin <logan.sparlin@gmail.com>
2015-07-27 22:00:45 -0500

    initial

---
+++ client/main.js
@@ -0,0 +1,16 @@
+Template.hello.onRendered(function() {
+  // var ww = $(window).width(),
+  //     wh = $(window).height(),
+  //     positionX = 0,
+  //     positionY = 0,
+  //     title = $('.CT0-title');
+
+  // $(window).on('mousemove', function(e) {
+  //   positionX = (((ww / 2) - e.pageX) / 100) - (title.outerWidth() / 2)
+  //   positionY = (((wh / 2) - e.pageY) / 50) - (title.outerHeight() / 2)
+  //   // title.css({
+  //   //   'transform': 'translate3d(' + positionX + 'px,' + positionY + 'px, 0)'
+  //   // });
+  //   TweenLite.to(title, 0.9, {x: positionX, y: positionY, force3D: true, autoRound: false, ease: Quart.easeOut});
+  // });
+});
